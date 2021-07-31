b70fa68d7fe673523ee587e96fadc8058841ca21
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-31 15:09:54 +0000

    updated invalidated map

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -5,7 +5,7 @@ $(() => {
     center: [48.42959706075791, -123.34509764072138],
     zoom: 13
   })
-  L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=4Z6KRmYugsIBUnw1Jpiy', {
+  L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=MDTfUhxpHfqM2YyggTW7', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }).addTo(map);
 });
